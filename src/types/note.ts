bf70f0c6b1e0f9193edfed5d97983cfde242831e
Andriy Romanov <mgm.agro04@gmail.com>
2025-06-13 17:52:11 +0300

    edit Note

--- src/types/note.ts
+++ src/types/note.ts
@@ -3,4 +3,6 @@ export interface Note {
   title: string;
   content: string;
   tag: "Work" | "Personal" | "Meeting" | "Shopping" | "Todo";
+  createdAt: string; // ISO-рядок
+  updatedAt?: string; // зробимо необов’язковим, якщо може бути відсутній
 }
