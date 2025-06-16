0a677527010a0bdc783d7f9712fd9d0c05590ba8
Andriy Romanov <mgm.agro04@gmail.com>
2025-06-17 00:58:27 +0300

    edit bug

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,28 +1,21 @@
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from "@tanstack/react-query";
+import { getSingleNote } from "@/lib/api";
 import NoteDetailsClient from "@/components/NoteDetails/NoteDetails.client";
-import { fetchNoteById } from "@/lib/api";
-import { HydrationBoundary, dehydrate, QueryClient } from "@tanstack/react-query";
-import type { Metadata } from "next";
-
-// ЦЕ ГОЛОВНЕ — правильна типізація
-type PageProps = {
-  params: {
-    id: string;
-  };
+type Props = {
+  params: Promise<{ id: string }>;
 };
 
-export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
-  return {
-    title: `Note ${params.id}`,
-  };
-}
-
-export default async function NoteDetailsPage({ params }: PageProps) {
-  const id = Number(params.id);
+const NoteDetails = async ({ params }: Props) => {
+  const { id } = await params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
+    queryFn: () => getSingleNote(id),
   });
 
   return (
@@ -30,4 +23,6 @@ export default async function NoteDetailsPage({ params }: PageProps) {
       <NoteDetailsClient />
     </HydrationBoundary>
   );
-}
+};
+
+export default NoteDetails;
